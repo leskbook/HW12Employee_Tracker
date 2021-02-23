39cbf78143d7803c38c59b0ad98eca023351e9fd
leskbook <lesliek.book@gmail.com>
2021-02-23 16:12:40 -0500

    Commit with working index.js but not all db functions working

---
+++ index.js
@@ -0,0 +1,330 @@
+'use strict';
+
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+
+
+const promptMessages = {
+    viewAllEmployees: "View All Employees",
+    viewByDepartment: "View All Employees By Department",
+    viewByManager: "View All Employees By Manager",
+    addEmployee: "Add An Employee",
+    removeEmployee: "Remove An Employee",
+    updateRole: "Update Employee Role",
+    updateEmployeeManager: "Update Employee Manager",
+    viewAllRoles: "View All Roles",    
+    exit: "Exit"
+};
+
+const connection = mysql.createConnection({
+    host: 'localhost',    
+    port: 3306,    
+    user: 'root',    
+    password: 'myP@ssword',
+    database: 'employees'
+});
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(chalk.greenBright('connected as id ' + connection.threadId));
+    prompt();
+
+});
+
+function prompt() {
+    inquirer
+        .prompt({
+            name: 'action',
+            type: 'list',
+            message: 'Select Option',
+            choices: [
+                promptMessages.viewAllEmployees,
+                promptMessages.viewByDepartment,
+                promptMessages.viewByManager,
+                promptMessages.viewAllRoles,
+                promptMessages.addEmployee,
+                promptMessages.removeEmployee,
+                promptMessages.updateRole,                
+                promptMessages.exit
+            ]
+        })
+        .then(answer => {
+            console.log('answer', answer);
+            switch (answer.action) {
+                case promptMessages.viewAllEmployees:
+                    viewAllEmployees();
+                    break;
+
+                case promptMessages.viewByDepartment:
+                    viewByDepartment();
+                    break;
+
+                case promptMessages.viewByManager:
+                    viewByManager();
+                    break;
+
+                case promptMessages.addEmployee:
+                    addEmployee();
+                    break;
+
+                case promptMessages.removeEmployee:
+                    remove('delete');
+                    break;
+
+                case promptMessages.updateRole:
+                    remove('role');
+                    break;
+
+                case promptMessages.viewAllRoles:
+                    viewAllRoles();
+                    break;              
+
+                case promptMessages.exit:
+                    connection.end();
+                    break;
+            }
+        });
+}
+
+function viewAllEmployees() {
+    const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role ON (role.id = employee.role_id)
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY employee.id;`;
+
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log('\n');
+        console.log(chalk.magentaBright.italic('VIEW ALL EMPLOYEES'));
+        console.log('\n');
+        console.table(res);
+        prompt();
+    });
+}
+
+function viewByDepartment() {
+    const query = `SELECT department.name AS department, role.title, employee.id, employee.first_name, employee.last_name
+    FROM employee
+    LEFT JOIN role ON (role.id = employee.role_id)
+    LEFT JOIN department ON (department.id = role.department_id)
+    ORDER BY department.name;`;
+
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log('\n');
+        console.log(chalk.magentaBright.italic('VIEW EMPLOYEE BY DEPARTMENT'));
+        console.log('\n');
+        console.table(res);
+        prompt();
+    });
+}
+
+
+function viewByManager() {
+    const query = `SELECT CONCAT(manager.first_name, ' ', manager.last_name) AS manager, department.name AS department, employee.id, employee.first_name, employee.last_name, role.title
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role ON (role.id = employee.role_id && employee.manager_id != 'NULL')
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY manager;`;
+
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log('\n');
+        console.log(chalk.magentaBright.italic('VIEW EMPLOYEE BY MANAGER'));
+        console.log('\n');
+        console.table(res);
+        prompt();
+    });
+}
+
+function viewAllRoles() {
+    const query = `SELECT role.title, employee.id, employee.first_name, employee.last_name, department.name AS department
+    FROM employee
+    LEFT JOIN role ON (role.id = employee.role_id)
+    LEFT JOIN department ON (department.id = role.department_id)
+    ORDER BY role.title;`;
+
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log('\n');
+        console.log(chalk.magentaBright.italic('VIEW EMPLOYEE BY ROLE'));
+        console.log('\n');
+        console.table(res);
+        prompt();
+    });
+
+}
+
+async function addEmployee() {
+    const addname = await inquirer.prompt(askName());
+    connection.query('SELECT role.id, role.title FROM role ORDER BY role.id;', async (err, res) => {
+        if (err) throw err;
+        const { role } = await inquirer.prompt([
+            {
+                name: 'role',
+                type: 'list',
+                choices: () => res.map(res => res.title),
+                message: 'Employee role?: '
+            }
+        ]);
+        let roleId;
+        for (const row of res) {
+            if (row.title === role) {
+                roleId = row.id;
+                continue;
+            }
+        }
+        connection.query('SELECT * FROM employee', async (err, res) => {
+            if (err) throw err;
+            let choices = res.map(res => `${res.first_name} ${res.last_name}`);
+            choices.push('none');
+            let { manager } = await inquirer.prompt([
+                {
+                    name: 'manager',
+                    type: 'list',
+                    choices: choices,
+                    message: 'Employee Manager: '
+                }
+            ]);
+            let managerId;
+            let managerName;
+            if (manager === 'none') {
+                managerId = null;
+            } else {
+                for (const data of res) {
+                    data.fullName = `${data.first_name} ${data.last_name}`;
+                    if (data.fullName === manager) {
+                        managerId = data.id;
+                        managerName = data.fullName;
+                        console.log(managerId);
+                        console.log(chalk.bgRedBright(managerName));
+                        continue;
+                    }
+                }
+            }
+            console.log(chalk.redBright('Employee has been added. View all employees to verify'));
+            connection.query(
+                'INSERT INTO employee SET ?',
+                {
+                    first_name: addname.first,
+                    last_name: addname.last,
+                    role_id: roleId,
+                    manager_id: parseInt(managerId)
+                },
+                (err, res) => {
+                    if (err) throw err;
+                    prompt();
+
+                }
+            );
+        });
+    });
+
+}
+function remove(input) {
+    const promptQ = {
+        yes: "yes I know the ID",
+        no: "no I don't",
+    };
+    inquirer.prompt([
+        {
+            name: "action",
+            type: "list",
+            message: "An Employee ID must be entered. View all employees to get" +
+                " the employee ID",
+            choices: [promptQ.yes, promptQ.no]
+        }
+    ]).then(answer => {
+        if (input === 'delete' && answer.action === "yes") removeEmployee();
+        else if (input === 'role' && answer.action === "yes") updateRole();
+        else viewAllEmployees();
+
+
+
+    });
+};
+
+async function removeEmployee() {
+
+    const answer = await inquirer.prompt([
+        {
+            name: "first",
+            type: "input",
+            message: "Enter ID of the employee to remove:  "
+        }
+    ]);
+
+    connection.query('DELETE FROM employee WHERE ?',
+        {
+            id: answer.first
+        },
+        function (err) {
+            if (err) throw err;
+        }
+    )
+    console.log(chalk.redBright('Employee has been removed'));
+    prompt();
+
+};
+
+function askId() {
+    return ([
+        {
+            name: "name",
+            type: "input",
+            message: "What is the employe ID?:  "
+        }
+    ]);
+}
+
+
+async function updateRole() {
+    const employeeId = await inquirer.prompt(askId());
+
+    connection.query('SELECT role.id, role.title FROM role ORDER BY role.id;', async (err, res) => {
+        if (err) throw err;
+        const { role } = await inquirer.prompt([
+            {
+                name: 'role',
+                type: 'list',
+                choices: () => res.map(res => res.title),
+                message: 'What is the employees new role?: '
+            }
+        ]);
+        let roleId;
+        for (const row of res) {
+            if (row.title === role) {
+                roleId = row.id;
+                continue;
+            }
+        }
+        connection.query(`UPDATE employee 
+        SET role_id = ${roleId}
+        WHERE employee.id = ${employeeId.name}`, async (err, res) => {
+            if (err) throw err;
+            console.log(chalk.redBright('Employee role updated')),
+            prompt();
+        });
+    });
+}
+
+function askName() {
+    return ([
+        {
+            name: "first",
+            type: "input",
+            message: "Enter first name: "
+        },
+        {
+            name: "last",
+            type: "input",
+            message: "Enter last name: "
+        }
+    ]);
+}
+
+
--- schema.sql
+++ schema.sql
@@ -1,47 +1,54 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
-USE employee_db;
-
-CREATE TABLE departments (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30)
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+USE employees;
+CREATE TABLE department (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
 );
-CREATE TABLE roles (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL,
-  departments_id INT,
-  FOREIGN KEY (departments_id) REFERENCES departments(id)
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
-CREATE TABLE employees (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  manager_id INT,
-  roles_id INT,
-  FOREIGN KEY (roles_id) REFERENCES roles(id),
-  FOREIGN KEY (manager_id) REFERENCES employees(id)
+CREATE TABLE employee (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE
+  SET
+    NULL
 );
-INSERT INTO departments (name) VALUE ("Engineering");
-INSERT INTO departments (name) VALUE ("Legal");
-INSERT INTO departments (name) VALUE ("Finance");
-INSERT INTO departments (name) VALUE ("Sales");
-INSERT INTO roles (title, salary, departments_id) VALUE ("Lead Engineer", 150000, 1);
-INSERT INTO roles (title, salary, departments_id) VALUE ("Software Engineer", 120000, 1);
-INSERT INTO roles (title, salary, departments_id) VALUE ("Legal Team Lead", 180000, 2);
-INSERT INTO roles (title, salary, departments_id) VALUE ("Lawyer", 190000, 2);
-INSERT INTO roles (title, salary, departments_id) VALUE ("Accountant", 125000, 3);
-INSERT INTO roles (title, salary, departments_id) VALUE ("CPA", 90000, 3);
-INSERT INTO roles (title, salary, departments_id) VALUE ("Sales Lead", 100000, 4);
-INSERT INTO roles (title, salary, departments_id) VALUE ("Salesperson", 80000, 1);
-INSERT INTO employees (first_name, last_name, manager_id, roles_id) VALUE ("John", "Doe", null, 2);
-INSERT INTO employees (first_name, last_name, manager_id, roles_id) VALUE ("Walter", "White", 1, 1);
-INSERT INTO employees (first_name, last_name, manager_id, roles_id) VALUE ("Jessie", "Pinkman", 1, 4);
-INSERT INTO employees (first_name, last_name, manager_id, roles_id) VALUE ("Skylar", "White", 1, 3);
-INSERT INTO employees (first_name, last_name, manager_id, roles_id) VALUE ("Saul", "Goodman", 1, 2);
-SELECT *
-FROM departments;
-SELECT *
-FROM roles;
-SELECT *
-FROM employees;
+use employees;
+INSERT INTO
+  department (name)
+VALUES
+  ('Engineering'),
+  ('Finance'),
+  ('Sales'),
+  ('Legal');
+INSERT INTO
+  role (title, salary, department_id)
+VALUES
+  ('Lead Engineer', 150000, 1),
+  ('Software Engineer', 120000, 1),
+  ('CFO', 225000, 2),
+  ('CPA', 80000, 2),
+  ('Sales Manager', 70000, 3),
+  ('Sales Associate', 30000, 3),
+  ('Lawyer', 130000, 4),
+  ('Law Associate', 80000, 4);
+INSERT INTO
+  employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Walter', 'White', 1, NULL),
+  ('Jessie', 'Pinkman', 2, 1),
+  ('Skylar', 'White', 3, NULL),
+  ('Saul', 'Goodman', 4, 3);
